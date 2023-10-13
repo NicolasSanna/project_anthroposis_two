e41eb141da2965d4b828069a3d41984124cc1c56
NicolasSanna <nico13sanna@gmail.com>
2023-10-13 17:24:17 +0200

    Correction du base.html.twig

--- database/Stored Procedures/SELECT/SP_SearchEngine.sql
+++ database/Stored Procedures/SELECT/SP_SearchEngine.sql
@@ -8,7 +8,7 @@ BEGIN
     -- Création de la variable locale à la procéédure stockée v_srch et assignation de sa valeur au paramètre entré en concaténant les caractères % % correspondant à la recherche avant et après la chaîne de caractères entourées en SQL.
     DECLARE v_srch VARCHAR(255) DEFAULT CONCAT('%', v_search, '%');
 
-    SELECT u.pseudo, a.title, a.description, a.slug, DATE_FORMAT(a.created_at, '%d/%m /%Y à %H:%i') AS created_at_fr
+    SELECT u.pseudo, a.title, a.description, a.slug, DATE_FORMAT(a.created_at, '%d/%m/%Y à %H:%i') AS created_at_fr
     FROM article AS a
     INNER JOIN user AS u ON a.user_id = u.id
     -- Précision de l'ordonnancement et du regroupement des caractères de manière explicite (COLLATE)
